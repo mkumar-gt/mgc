5ee3d1a039673783b824bafc95b91f12fdeb7297
Muthu Kumar <muth_ukumar@yahoo.com>
2018-12-23 16:28:06 +0530

    C5

--- the_app/forms.py
+++ the_app/forms.py
@@ -1,10 +1,39 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from the_app.models import User
 
-class myForm(FlaskForm):
+class LoginForm(FlaskForm):
 	
-	username = StringField('Username', validators=[DataRequired(message="something is wrong here")])
+	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Submit')
+
+class RegistrationForm(FlaskForm):
+	
+	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email Address', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField('Password Again', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('This username already exists')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('This email already exists!')
+
+	#doing the following code was a failure:: 
+
+	#def validate_password2(self, password2):
+		
+
+	# 	if password2 != password:
+	# 		raise ValidationError("The passwords don't match!")
+
+	# somehow I have to pass in password.data into this function.
--- the_app/routes.py
+++ the_app/routes.py
@@ -1,11 +1,15 @@
-from the_app import app
-from flask import render_template, flash, redirect
-from the_app.forms import myForm
+from the_app import app, db
+from flask import render_template, flash, redirect, request, url_for
+from the_app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from the_app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-	user = {'username': 'Mu'}
+	user = current_user
 
 	posts = [
 		{'author': {'username': 'Jonathan', 'nickname': 'John'},
@@ -20,9 +24,38 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-		form = myForm()
+		
+		if current_user.is_authenticated:
+			return redirect(url_for('index'))
+		form = LoginForm()
 		if form.validate_on_submit():
-			flash('Login requested for user {}, remember me is {}'.format(form.username.data, form.remember_me.data))
-			return redirect('/index')
+			user = User.query.filter_by(username=form.username.data).first()
+			if user is None or not user.check_password_hash(form.password.data):
+				flash('Invalid username or password')
+				return redirect(url_for('login'))
+			login_user(user, remember=form.remember_me.data)
+			next_page = request.args.get('next')
+			if not next_page or url_parse(next_page).netloc != '':
+				next_page = url_for('index')
+			return redirect(next_page)
+		return render_template('login.html', title='Sign In', form=form)
 
-		return render_template('login.html', title='Sign In', form=form)	
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash("You're now registered")
+		return redirect(url_for('login'))
+	return render_template('register.html', title="Register", form=form)
+
