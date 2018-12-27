399919d2f1ad19ffda58400830dc9e9e69002a2f
Muthu Kumar <muth_ukumar@yahoo.com>
2018-12-27 23:55:52 +0530

    C6

--- the_app/forms.py
+++ the_app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from the_app.models import User
 
 class LoginForm(FlaskForm):
@@ -37,3 +37,8 @@ class RegistrationForm(FlaskForm):
 	# 		raise ValidationError("The passwords don't match!")
 
 	# somehow I have to pass in password.data into this function.
+
+class EditProfileForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	about_me = TextAreaField('About Me:', validators=[Length(min=0, max=140)])
+	submit = SubmitField('Submit')
